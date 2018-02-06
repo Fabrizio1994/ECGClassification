7ed5edda7f5a3a494630cede214da9685ecd7d10
alescire94 <alescire94@gmail.com>
2018-02-06 11:48:10 +0100

    Knn and 2Means GridSearch for QRS detection

--- GridSearch.py
+++ GridSearch.py
@@ -1,70 +1,93 @@
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
-
 import pprint as pp
 
 
-############################################
+class GridSearch:
+    def __init__(self, features, labels):
+        X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.20)
+        kNN_classifier = KNeighborsClassifier()
+        kMeans_classifier = KMeans(n_clusters=2)
+        # With a Pipeline object we can assemble several steps
+        # that can be cross-validated together while setting different parameters.
+        pipelines = [kNN_classifier, kMeans_classifier]
 
-class GridSearch():
-    def __init__(self, classifier, params, X, target, target_names):
-
-        X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.20)
+        parameters_kNN = {
+            'n_neighbors': [1, 3, 5, 7, 9],
+            'weights': ['uniform', 'distance'],
+            'p': [1, 2]
+        }
+        parameters = [parameters_kNN, kMeans_classifier.get_params()]
         ## Create a Grid-Search-Cross-Validation object
-        ## to find in an automated fashion the best combination of parameters.
-        grid_search = GridSearchCV(classifier,
-                                   params,
-                                   scoring=metrics.make_scorer(metrics.f1_score, average='weighted'),
-                                   cv=5,
-                                   n_jobs=-1,
-                                   verbose=10)
+        ## to find in an automated fashion the best combination of parameters
 
-        ## Start an exhaustive search to find the best combination of parameters
-        ## according to the selected scoring-function.
-        print
-        grid_search.fit(X_train, y_train)
-        print
+        for i in range(len(parameters)):
+            grid_search = GridSearchCV(pipelines[i],
+                                       parameters[i],
+                                       scoring=metrics.make_scorer(metrics.f1_score, average='weighted'),
+                                       cv=5,
+                                       n_jobs=-1,
+                                       verbose=10)
+            ## Start an exhaustive search to find the best combination of parameters
+            ## according to the selected scoring-function
 
-        ## Print results for each combination of parameters.
-        number_of_candidates = len(grid_search.cv_results_['params'])
-        print("Results:")
-        for i in range(number_of_candidates):
-            print(i, 'params - %s; mean - %0.3f; std - %0.3f' %
-                  (grid_search.cv_results_['params'][i],
-                   grid_search.cv_results_['mean_test_score'][i],
-                   grid_search.cv_results_['std_test_score'][i]))
-        print
-        print("Best Parameters:")
-        pp.pprint(grid_search.best_params_)
-        print
-        print("Used Scorer Function:")
-        pp.pprint(grid_search.scorer_)
-        print
-        print("Number of Folds:")
-        pp.pprint(grid_search.n_splits_)
-        print
+            grid_search.fit(X_train, y_train)  ## Print results for each combination of parameters.
+            number_of_candidates = len(grid_search.cv_results_['params'])
+            print("Results:")
+            for j in range(number_of_candidates):
+                print(j, 'params - %s; mean - %0.3f; std - %0.3f' %
+                      (grid_search.cv_results_['params'][j],
+                       grid_search.cv_results_['mean_test_score'][j],
+                       grid_search.cv_results_['std_test_score'][j]))
 
-        # Let's train the classifier that achieved the best performance,
-        # considering the select scoring-function,
-        # on the entire original TRAINING-Set
-        Y_predicted = grid_search.predict(X_test)
+            print("Best Estimator:")
+            pp.pprint(grid_search.best_estimator_)
 
-        # Evaluate the performance of the classifier on the original Test-Set
-        output_classification_report = metrics.classification_report(
-            y_test,
-            Y_predicted,
-            target_names=target_names)
-        print
-        print
-        "----------------------------------------------------"
-        print(output_classification_report)
-        print
-        "----------------------------------------------------"
-        print
+            print("Best Parameters:")
+            pp.pprint(grid_search.best_params_)
 
-        # Compute the confusion matrix
-        confusion_matrix = metrics.confusion_matrix(y_test, Y_predicted)
-        print
-        print("Confusion Matrix: True-Classes X Predicted-Classes")
-        print(confusion_matrix)
+            print("Used Scorer Function:")
+            pp.pprint(grid_search.scorer_)
+
+            print("Number of Folds:")
+            pp.pprint(grid_search.n_splits_)
+
+            # Let's train the classifier that achieved the best performance,
+            # considering the select scoring-function,
+            # on the entire original TRAINING-Set
+            Y_predicted = grid_search.predict(X_test)
+            target_names = ['peak', 'not peak']
+            # Evaluate the performance of the classifier on the original Test-Set
+            output_classification_report = metrics.classification_report(
+                y_test,
+                Y_predicted,
+                target_names=target_names)
+
+            print("----------------------------------------------------")
+            print(output_classification_report)
+            print("----------------------------------------------------")
+
+            # Compute the confusion matrix
+            confusion_matrix = metrics.confusion_matrix(y_test, Y_predicted)
+
+            print("Confusion Matrix: True-Classes X Predicted-Classes")
+            print(confusion_matrix)
+
+            # Compute the Normalized-accuracy
+            normalized_accuracy = metrics.accuracy_score(y_test, Y_predicted)
+
+            print("Normalized Accuracy: ")
+            print(normalized_accuracy)
+
+            # Compute the Matthews Corrcoef value
+            f1_score = metrics.f1_score(y_test, Y_predicted)
+
+            print("f1 score: ")
+            print(f1_score)
+
+
+
--- Main.py
+++ Main.py
@@ -1,16 +1,8 @@
 from FeatureExtraction import FeatureExtraction
-from sklearn.neighbors import KNeighborsClassifier
 from GridSearch import GridSearch
 
 fe = FeatureExtraction()
 
 features, labels = fe.extract_features('100')
-knn = KNeighborsClassifier()
-knn_parameters = {'n_neighbors': [1, 3, 5, 7, 9],
-                  'weights': ['uniform', 'distance'],
-                  'p': [1, 2],
-                  }
-
-target_names = ['peak', 'not-peak']
-gs = GridSearch(knn, knn_parameters, features, labels, target_names)
+gs = GridSearch(features, labels)
 
