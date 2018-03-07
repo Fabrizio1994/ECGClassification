rpeakevaluation + reports. knn fixed window (see beat average).

FeatureExtraction.py
@@ -4,50 +4,48 @@ import numpy as np
 import os
 
 class FeatureExtraction:
-    def extract_features(self, sample_name, split_size=650000):
+    def extract_features(self, sample_name, annotation_type, window_size):
         print("Extracting features for signal " + sample_name + "...")
         record = wfdb.rdrecord(sample_name)
-        annotation = wfdb.rdann(sample_name, 'atr')
+        annotation = wfdb.rdann('annotations/' + annotation_type + '/' + sample_name.replace("sample/", "") , 'atr')
         first_channel = []
         second_channel = []
-        for elem in record.p_signal[:split_size]:
+        for elem in record.p_signal:
             first_channel.append(elem[0])
             second_channel.append(elem[1])
         filtered_first_channel = self.passband_filter(first_channel)
         filtered_second_channel = self.passband_filter(second_channel)
         gradient_channel1 = self.normalized_gradient(filtered_first_channel)
         gradient_channel2 = self.normalized_gradient(filtered_second_channel)
-        print("actual peaks:"+ str(len(annotation.sample)))
+        print("actual peaks:" + str(len(annotation.sample)))
 
-        return self.compute_features(gradient_channel1, gradient_channel2, annotation)
+        return self.compute_features(gradient_channel1, gradient_channel2, annotation, window_size)
 
-    def compute_features(self, channel1, channel2, annotation):
+    def compute_features(self, channel1, channel2, annotation, window_size):
         features = []
         for i in range(len(channel1)):
             features.append([channel1[i], channel2[i]])
         samples = annotation.sample
         labels = [-1]*len(channel1)
         for j in range(len(samples)):
-            qrs_region = self.get_qrs_region(samples, j)
+            qrs_region = self.get_qrs_region(samples, j, window_size)
             for sample in qrs_region:
                 labels[sample] = 1
         return np.asarray(features), np.asarray(labels)
 
-    def get_qrs_region(self, samples, location):
-        if location < len(samples)-6:
-            return [q for q in range(samples[location]-5, samples[location]+6)]
+    def get_qrs_region(self, samples, location, window_size):
+        if location < len(samples) - (int(window_size/2) + 1):
+            return [q for q in range(samples[location] - (int(window_size/2)), samples[location] +
+                                     int(window_size/2)+1)]
         else:
-            gap = len(samples) - location -1
-            return [q for q in range(samples[location]- gap, samples[location]+gap)]
+            gap = len(samples) - location - 1
+            return [q for q in range(samples[location] - gap, samples[location]+gap)]
 
     def normalized_gradient(self, channel):
         gradient = np.diff(channel)
-        #gradient = []
-        #gradient = np.gradient(channel)
         gradient_norm = np.sqrt(np.sum(np.square(gradient)))
-        #gradient_norm = max(gradient)
-        normalized = np.divide(gradient, gradient_norm)
-        return normalized
+        normalized_gradient = np.divide(gradient, gradient_norm)
+        return normalized_gradient
 
     def overwrite_signal(self, first_channel, second_channel, record):
         for i in range(len(record.p_signal)):
@@ -57,21 +55,8 @@ class FeatureExtraction:
 
     def passband_filter(self, channel):
         freq = 360.0/2.0
-        b,a = signal.butter(1,[5/freq, 12/freq], btype="band")
-        #b, a = signal.butter(2, 11/freq, btype='lowpass')
-        #d, c = signal.butter(1, 5/freq, btype='highpass')
-        #new_channel = signal.lfilter(d, c, signal.lfilter(b, a, channel))
-        #new_channel = signal.filtfilt(b,a, signal.filtfilt(d, c, channel))
+        b, a = signal.butter(1, [5/freq, 12/freq], btype="band")
         new_channel = signal.filtfilt(b, a, channel)
-        return new_channel
-
-    def func_filter(self, channel):
-        d = [-1, 32, 1]
-        c = [1, 1]
-        b = [1, -2, 1]
-        a = [1, -2, 1]
-        #new_channel = signal.filtfilt(b, a, signal.filtfilt(d, c, channel))
-        new_channel = signal.lfilter(b, a, signal.lfilter(d, c, channel))
         return new_channel
 
     def extract_from_all(self,split_size):

RPeakEvaluation.py
@@ -1,10 +1,9 @@
 import os
 from KNN import KNN
 from FeatureExtraction import FeatureExtraction
-from Utility import Utility
 import wfdb
+import numpy as np
 
-ut = Utility()
 fe = FeatureExtraction()
 knn = KNN()
 
@@ -15,22 +14,34 @@ class RPeakEvaluation:
     SIG_LEN = 650000
 
     def validate_r_peak(self):
-        for name in os.listdir("features"):
-            if name.endswith('.tsv'):
-                signame = name.replace(".tsv", "")
+        window_sizes = [10, 20, 50]
+        annotation_type = ['beat', 'cleaned']
+        for name in os.listdir("sample"):
+            if name.endswith('.atr'):
+                signame = name.replace(".atr", "")
                 print(signame)
-                annotation = wfdb.rdann("sample/"+signame, 'atr')
-                locations = list(filter(lambda x: x > (self.SIG_LEN - self.SIZE_LAST_20), annotation.sample))
-                prediction1 = self.get_predictions(signame, 0)
-                prediction2 = self.get_predictions(signame, 1)
-                self.evaluate_prediction(prediction1, locations, signame, 1, self.SIZE_LAST_20)
-                self.evaluate_prediction(prediction2, locations, signame, 2, self.SIZE_LAST_20)
+                for type in annotation_type:
+                    annotation = wfdb.rdann("annotations/"+ type + '/' + signame, 'atr')
+                    locations = list(filter(lambda x: x > (self.SIG_LEN - self.SIZE_LAST_20), annotation.sample))
 
-    def get_predictions(self, signame, n_channel, total_size=SIG_LEN,
+                    for size in window_sizes:
+                        prediction = self.get_predictions(signame, 0, window_size=size)
+                        labels = self.get_labels(locations, size)
+                        self.evaluate_prediction(prediction, labels, signame,self.SIZE_LAST_20, window_size = size,
+                                                 annotation_type=type, classifier="RPeakDetection")
+
+
+    def get_labels(self, locations, window_size):
+        labels = []
+        interval = [q for q in range(int(-window_size/2), int(window_size/2) + 1)]
+        for loc in locations:
+            labels.append([loc + q for q in interval])
+        return labels
+
+    def get_predictions(self, signame, n_channel, window_size, total_size=SIG_LEN,
                         test_size=SIZE_LAST_20):
         record = wfdb.rdrecord('sample/' + signame)
         channel = []
-        window = 10
         for elem in record.p_signal:
             channel.append(elem[n_channel])
         prediction = []
@@ -38,12 +49,12 @@ class RPeakEvaluation:
         for line in file:
             value = int(line.replace("\n", ""))
             if value > total_size - test_size:
-                real_peak_index = self.get_r_peak(channel, value, window)
+                real_peak_index = self.get_r_peak(channel, value, window_size)
                 prediction.append(real_peak_index)
         return prediction
 
-    def get_r_peak(self, channel, value, window):
-        indexes = range(int(value-window/2), int(value+window/2+1))
+    def get_r_peak(self, channel, value, window_size):
+        indexes = range(int(value-window_size/2), int(value+window_size/2+1))
         max = abs(channel[value])
         rpeak = value
         for index in indexes:
@@ -52,30 +63,31 @@ class RPeakEvaluation:
                 rpeak = index
         return rpeak
 
-    def evaluate_prediction(self, prediction, locations, signame, channel_number, length):
+    def evaluate_prediction(self, prediction, labels, signame, length, window_size, annotation_type, classifier):
         TP = 0
         FP = 0
         FN = 0
 
-        i = 0
-        j = 0
-        while i < len(prediction) and j < len(locations):
-            qrs_region = [q for q in range(locations[j] - 5, locations[j] + 6)]
-            if prediction[i] in qrs_region:
+        for pred in prediction:
+            if pred in np.concatenate(labels):
                 TP += 1
-                i += 1
-                j += 1
-            elif prediction[i] > locations[j]:
-                FN += 1
-                j += 1
             else:
                 FP += 1
-                i += 1
 
-        FN += len(locations) - j
-        FP += len(prediction) - i
+        for interval in labels:
+            found = False
+            for value in interval:
+                if value in prediction:
+                    found = True
+            if not found:
+                FN += 1
 
         TN = length - TP - FP - FN
-        file = open("report_"+str(channel_number)+".tsv", "a")
+
+        DER = ((FP + FN) / TP)
+        SE = (TP / (TP + FN)) * 100
+        P = (TP / (TP + FP)) * 100
+        file = open("reports/" + classifier + "/" + annotation_type + "_" + str(window_size) + ".tsv", "a")
         file.write("%s\n" %(signame))
-        file.write("TP:%s\tTN:%s\tFP:%s\tFN:%s\n" % (str(TP), str(TN), str(FP), str(FN)))
+        file.write("TP:%s\tTN:%s\tFP:%s\tFN:%s\tDER:%s\tSE:%s\tP:%s\n" % (str(TP), str(TN), str(FP), str(FN), str(DER),
+                                                                          str(SE), str(P)))

Utility.py
@@ -3,14 +3,23 @@ import numpy as np
 import os
 from GridSearch import GridSearch
 from FeatureExtraction import FeatureExtraction
+from KNN import KNN
+from RPeakEvaluation import RPeakEvaluation
 
+SIG_LEN = 650000
+
+rpe = RPeakEvaluation()
+knn = KNN()
 fe = FeatureExtraction()
+gs = GridSearch()
 
 class Utility:
 
     NON_BEAT_ANN = [ '[', '!', ']', 'x', '(', ')', 'p', 't', 'u', '`', '\'', '^', '|', '~', '+', 's', 'T', '*', 'D', '='
-        ,'"', '@']
-    WINDOW = 10
+        , '"', '@']
+    CLEANING_WINDOW = 10
+    WINDOW_SIZES = [10, 20, 50]
+    ANNOTATION_TYPES = ['beat', 'cleaned']
 
     def clean_signal(self, sample_name):
         print(sample_name)
@@ -44,7 +53,7 @@ class Utility:
         new_symbol = []
         for j in range(len(samples)):
                 annotated = samples[j]
-                qrs_window = [q for q in range(-int(self.WINDOW/2),int(self.WINDOW/2) + 1)]
+                qrs_window = [q for q in range(-int(self.CLEANING_WINDOW / 2), int(self.CLEANING_WINDOW / 2) + 1)]
                 qrs_values = [channel[annotated+q] for q in qrs_window]
                 qrs_indexes = [annotated + q for q in qrs_window]
                 index_max = qrs_indexes[0]
@@ -58,48 +67,24 @@ class Utility:
         return new_sample, new_symbol
 
     def clean_all(self):
-        for signal_name in os.listdir("features"):
-            self.clean_signal(signal_name.replace(".tsv",""))
+        for signal_name in os.listdir("sample"):
+            if signal_name.endswith('.atr'):
+                self.clean_signal(signal_name.replace(".atr",""))
 
     def remove_non_beat_for_all(self):
         for signal_name in os.listdir("sample"):
             if signal_name.endswith(".atr"):
                 name = signal_name.replace(".atr","")
                 new_sample, new_symbol = self.remove_non_beat("sample/"+name)
-                wfdb.wrann(name,"atr", np.asarray(new_sample), np.asarray(new_symbol))
-
-    def read_all(self):
-        features = []
-        labels = []
-        for name in os.listdir("features"):
-            print("reading signal "+name.replace(".tsv",""))
-            file = open("features/"+name,"r")
-            for line in file:
-                vector = line.split("\t")
-                features.append([float(vector[0]), float(vector[1])])
-                labels.append(int(vector[2].replace("\n","")))
-            file.close()
-        return np.asarray(features), np.asarray(labels)
-
-    def read_signal(self, name):
-        features = []
-        labels = []
-        file = open("features/" + name, "r")
-        for line in file:
-            vector = line.split("\t")
-            features.append([float(vector[0]), float(vector[1])])
-            labels.append(int(vector[2].replace("\n", "")))
-        return np.asarray(features), np.asarray(labels)
+                wfdb.wrann(name, "atr", np.asarray(new_sample), np.asarray(new_symbol))
+                os.system("mv " + signal_name + " annotations/beat")
 
     def write_csv_signal(self, signal_name):
-        file1 = open("csv/"+signal_name+"_1.csv", "w")
-        file2 = open("csv/"+signal_name+"_2.csv", "w")
-        record = wfdb.rdrecord("sample/"+signal_name)
+        file = open("csv/" + signal_name + ".csv", "w")
+        record = wfdb.rdrecord("sample/" + signal_name)
         for elem in record.p_signal:
-            file1.write("%s\n" % (str(elem[0])))
-            file2.write("%s\n" % (str(elem[1])))
-        file1.close()
-        file2.close()
+            file.write("%s\n" % (str(elem[0])))
+        file.close()
 
     def get_command(self, file_name):
         return "python2 RPeakDetection.py 360  < csv/" + file_name+ " > rpeak_output/" + file_name
@@ -109,8 +94,17 @@ class Utility:
             os.system(self.get_command(file_name))
 
     def run_knn(self):
-        for name in os.listdir("features"):
-            signal_name = name.replace(".tsv", "")
-            train_features, train_labels = fe.extract_features("sample/" + signal_name)
-            GridSearch(signal_name, train_features, train_labels, ["not QRS", "QRS"])
-
+        for name in os.listdir("sample"):
+            if name.endswith('.atr'):
+                signal_name = name.replace(".atr", "")
+                for type in self.ANNOTATION_TYPES:
+                    annotation = wfdb.rdann('annotations/' + type + '/' + signal_name, 'atr')
+                    for size in self.WINDOW_SIZES:
+                        train_features, train_labels = fe.extract_features("sample/" + signal_name, type, size)
+                        prediction = gs.grid_search(train_features, train_labels)
+                        cleaned_prediction = knn.clean_prediction(prediction)
+                        locations = knn.get_index(cleaned_prediction)
+                        print(locations)
+                        exit()
+                        labels = rpe.get_labels(annotation.sample, size)
+                        rpe.evaluate_prediction(locations, labels, name, SIG_LEN, size, type, classifier="KNN")