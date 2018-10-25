94a212c15d026b1b2a0244d5baa5935f0406e66f
alescire94 <alescire94@gmail.com>
2018-10-25 16:53:55 +0200

    KNN Feature Extraction optimized

--- rpeakdetection/KNN/KNN.py
+++ rpeakdetection/KNN/KNN.py
@@ -23,13 +23,13 @@ class KNN:
 
     DB = "mitdb"
 
-    def rpeak_detection(self,names=None):
+    def rpeak_detection(self,names=None, write=True, train=True):
         window_size = 100
         test_size = 0.97
         min_dist = 72
-        approach_f = ['KNN_s']
-        channels_f = ['1','12']
-        filtered_f = ['RS']
+        approach_f = ['KNN_w', 'KNN_s']
+        channels_f = ['1', '2', '12']
+        filtered_f = ['FS', 'RS']
         results = defaultdict(list)
         combinations = [approach_f, channels_f, filtered_f]
         if names is None:
@@ -44,14 +44,15 @@ class KNN:
                 path = ("data/ecg/"+self.DB+"/"+name)
                 rpeak_locations = ut.remove_non_beat(path, rule_based=False)[0]
                 record, X, Y = fe.extract_features(name=name,path=path, rpeak_locations=rpeak_locations,
-                                                   features_comb=comb)
+                                                   features_comb=comb, write=write)
                 X_train, X_test, y_train, y_test = train_test_split(X, Y, shuffle=False,
                                                                     test_size=test_size)
-                train = not('KNN_w' in comb and '1' in comb and 'FS' in comb)
+                #train = 'KNN_s' in comb and 'RS' in comb and '1' in comb
                 if train:
                     self.start_time, predicted = gs.predict(X_train, X_test, y_train, name, comb)
                 else:
-                    loaded_model = pickle.load(open('rpeakdetection/KNN/classifiers/knn_'+name+'.pkl', 'rb'))
+                    class_name = comb[0] + '_' + comb[1] + '_' + comb[2]
+                    loaded_model = pickle.load(open('rpeakdetection/KNN/classifiers/'+name+'_'+class_name+'.pkl', 'rb'))
                     self.start_time = time.time()
                     predicted = loaded_model.predict(X_test)
                 if 'KNN_s' in comb:
@@ -63,6 +64,8 @@ class KNN:
                     test_index = len(X_train) * window_size
                     elapsed_time, peaks = self.get_peaks(predicted, window_size, record, test_index, min_dist)
                 recall, precision = rpd.evaluate(peaks, path, window_size, False, test_index)
+                print(recall)
+                print(precision)
                 recalls.append(recall)
                 precisions.append(precision)
                 times.append(elapsed_time)
@@ -70,7 +73,7 @@ class KNN:
             results[comb_name] = [np.mean(precisions), np.mean(recalls), np.mean(times)]
             print("{:s}, {:f}, {:f}, {:f}".format(comb_name, np.mean(precisions), np.mean(recalls), np.mean(times)))
         print(results)
-        with open('results.pkl', 'wb') as fid:
+        with open('results_KNN.pkl', 'wb') as fid:
             pickle.dump(results, fid)
 
     def get_sample_peaks(self, predicted, test_index, min_dist):
@@ -108,4 +111,4 @@ class KNN:
 
 if __name__ == '__main__':
     knn = KNN()
-    knn.rpeak_detection()
+    knn.rpeak_detection(write=True, train=True)
