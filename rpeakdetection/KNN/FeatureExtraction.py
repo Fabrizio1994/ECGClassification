SSK algorithm evaluation fixed for multiple peaks inside the same QRS. Now precision and recall in [0,1]

rpeakdetection/KNN/FeatureExtraction.py
@@ -10,7 +10,7 @@ class FeatureExtraction:
         if 'KNN_w' in features_comb:
             window_size = 100
         else:
-            window_size = 2
+            window_size = 1
         if '1' in features_comb:
             channels = [0]
         elif '2' in features_comb:
@@ -24,14 +24,14 @@ class FeatureExtraction:
         if filtered:
             filtered_record = list()
             for i in range(len(channels)):
-                filtered_channel = self.filter(record[i])
+                filtered_channel = self.filter(record[i], features_comb)
                 filtered_record.append(filtered_channel)
             record = filtered_record
         comb_name = features_comb[0] + '_' + features_comb[1] + '_' + features_comb[2]
         feat_name = features_path + name + '_' + comb_name + '.pkl'
         labels_name = features_path + name + '_' + comb_name + '_labels.pkl'
         if write:
-            features, labels = self.compute_features(record, rpeak_locations, window_size=window_size)
+            features, labels = self.compute_features(record, rpeak_locations, comb=features_comb,window_size=window_size)
             with open(feat_name, 'wb') as fid:
                 pickle.dump(features, fid)
             with open(labels_name, 'wb') as fid:
@@ -54,18 +54,27 @@ class FeatureExtraction:
         record = np.transpose(record.p_signal)
         return record
 
-    def compute_features(self, record, rpeak_locations, window_size):
+    def compute_features(self, record, rpeak_locations,comb, window_size):
         sig_len = len(record[0])
         n_channels = len(record)
         n_regions = int(sig_len / window_size)
         # split record in regions of size window_size. If more then 1 channel, concatenates the regions
         features = np.concatenate(np.reshape(record,(n_channels,n_regions,window_size)), axis=1)
-        indexes = np.split(np.arange(sig_len), n_regions)
-        labels = list(map(lambda x: int(len(set(x).intersection(set(rpeak_locations)))!=0), indexes))
+        if 'KNN_w' in comb:
+            indexes = np.split(np.arange(sig_len), n_regions)
+            labels = list(map(lambda x: int(len(set(x).intersection(set(rpeak_locations)))!=0), indexes))
+        else:
+            half_qrs_width = 18
+            labels = np.zeros(sig_len, dtype=np.int8)
+            for peak in rpeak_locations:
+                for q in range(peak-half_qrs_width, peak+half_qrs_width):
+                    # avoids underflow and overflow
+                    if 0 <= q < sig_len:
+                        labels[q] = 1
         return features, labels
 
 
-    def filter(self, record):
+    def filter(self, record, comb):
         fs = 360
         # cutoff low frequency to get rid of baseline wonder
         f1 = 5
@@ -77,15 +86,21 @@ class FeatureExtraction:
         [a, b] = signal.butter(N, Wn, 'band')
         # filtering
         filtered = signal.filtfilt(a, b, record)
-        vector = [1, 2, 0, -2, -1]
-        int_c = 160 / fs
-        # 5.1 since in signal 100 we must include 5
-        b = interp1d(range(1, 6), [i * fs / 8 for i in vector])(np.arange(1, 5.1, int_c))
-        ecg_d = signal.filtfilt(b, 1, filtered)
-        # print(ecg_d[:5])
-        ecg_d = ecg_d / np.max(ecg_d)
-        ''' Squaring nonlinearly enhance the dominant peaks '''
-        ecg_s = ecg_d ** 2
-        return ecg_s
+        if 'KNN_w' in comb:
+            vector = [1, 2, 0, -2, -1]
+            int_c = 160 / fs
+            # 5.1 since in signal 100 we must include 5
+            b = interp1d(range(1, 6), [i * fs / 8 for i in vector])(np.arange(1, 5.1, int_c))
+            ecg_d = signal.filtfilt(b, 1, filtered)
+            # print(ecg_d[:5])
+            ecg_d = ecg_d / np.max(ecg_d)
+            ''' Squaring nonlinearly enhance the dominant peaks '''
+            ecg_s = ecg_d ** 2
+            return ecg_s
+        else:
+            ecg_d = [0]
+            diff = np.diff(filtered)
+            ecg_d.extend(diff)
+            return ecg_d
 
 

rpeakdetection/KNN/GridSearch.py
@@ -8,7 +8,6 @@ import time
 class GridSearch:
 
     def predict(self, X_train, X_test, y_train, name, comb):
-
         classifier = KNeighborsClassifier()
         parameters = {
             'n_neighbors': np.arange(1,21, 2),
@@ -33,4 +32,23 @@ class GridSearch:
         predicted = grid_search.predict(X_test)
         return start_time, predicted
 
+    def SSK_train(self, X_train, y_train, comb):
+        classifier = KNeighborsClassifier()
+        parameters = {
+            'n_neighbors': np.arange(1, 11, 2),
+            'p': [1, 2, 3],
+        }
+        grid_search = GridSearchCV(classifier,
+                                   parameters,
+                                   scoring=metrics.make_scorer(metrics.accuracy_score),
+                                   cv=5,
+                                   n_jobs=-1,
+                                   verbose=0)
+        print("training")
+        grid_search.fit(X_train, y_train)
+        class_name = 'rpeakdetection/KNN/classifiers/SSK_'+comb[1]+'_'+comb[2]+'.pkl'
+        best_classifier = grid_search.best_estimator_
+        with open(class_name, 'wb') as fid:
+            pickle.dump(best_classifier, fid)
+        return best_classifier
 

rpeakdetection/KNN/KNN.py
@@ -27,48 +27,21 @@ class KNN:
         window_size = 100
         test_size = 0.97
         min_dist = 72
-        approach_f = ['KNN_w', 'KNN_s']
-        channels_f = ['1', '2', '12']
+        evaluation_window_size = 36
+        approach_f = ['KNN_s']
+        channels_f = ['12', '1', '2']
         filtered_f = ['FS', 'RS']
         results = defaultdict(list)
         combinations = [approach_f, channels_f, filtered_f]
         if names is None:
             names = wfdb.get_record_list('mitdb')
         for comb in itertools.product(*combinations):
-            recalls = list()
-            precisions = list()
-            times = list()
             print(comb)
-        # we use the first 54s for training and the remaining 97% of the signal for testing
-            for name in names :
-                path = ("data/ecg/"+self.DB+"/"+name)
-                rpeak_locations = ut.remove_non_beat(path, rule_based=False)[0]
-                record, X, Y = fe.extract_features(name=name,path=path, rpeak_locations=rpeak_locations,
-                                                   features_comb=comb, write=write)
-                X_train, X_test, y_train, y_test = train_test_split(X, Y, shuffle=False,
-                                                                    test_size=test_size)
-                #train = 'KNN_s' in comb and 'RS' in comb and '1' in comb
-                if train:
-                    self.start_time, predicted = gs.predict(X_train, X_test, y_train, name, comb)
-                else:
-                    class_name = comb[0] + '_' + comb[1] + '_' + comb[2]
-                    loaded_model = pickle.load(open('rpeakdetection/KNN/classifiers/'+name+'_'+class_name+'.pkl', 'rb'))
-                    self.start_time = time.time()
-                    predicted = loaded_model.predict(X_test)
-                if 'KNN_s' in comb:
-                    elapsed_time = time.time() - self.start_time
-                    elapsed_time = elapsed_time / len(record[0])
-                    test_index = len(X_train)
-                    peaks = self.get_sample_peaks(predicted, test_index, min_dist)
-                if 'KNN_w' in comb:
-                    test_index = len(X_train) * window_size
-                    elapsed_time, peaks = self.get_peaks(predicted, window_size, record, test_index, min_dist)
-                recall, precision = rpd.evaluate(peaks, path, window_size, False, test_index)
-                print(recall)
-                print(precision)
-                recalls.append(recall)
-                precisions.append(precision)
-                times.append(elapsed_time)
+            if 'KNN_w' in comb:
+                precisions, recalls, times = self.QRS_KNN(comb, min_dist, names, test_size, train, window_size, write,
+                                                          evaluation_window_size)
+            else:
+                precisions, recalls, times = self.SSK(comb, names, write, train, evaluation_window_size)
             comb_name = comb[0] + '_' + comb[1] + '_' + comb[2]
             results[comb_name] = [np.mean(precisions), np.mean(recalls), np.mean(times)]
             print("{:s}, {:f}, {:f}, {:f}".format(comb_name, np.mean(precisions), np.mean(recalls), np.mean(times)))
@@ -76,16 +49,53 @@ class KNN:
         with open('results_KNN.pkl', 'wb') as fid:
             pickle.dump(results, fid)
 
-    def get_sample_peaks(self, predicted, test_index, min_dist):
-        all_peaks = np.where(predicted == 1)
-        all_peaks = list(map(lambda x: x + test_index, all_peaks))
+    def QRS_KNN(self, comb, min_dist, names, test_size, train, window_size, write, evaluation_window_size):
+        recalls = list()
+        precisions = list()
+        times = list()
+        for name in names:
+            path = ("data/ecg/" + self.DB + "/" + name)
+            rpeak_locations = ut.remove_non_beat(path, rule_based=False)[0]
+            record, X, Y = fe.extract_features(name=name, path=path, rpeak_locations=rpeak_locations,
+                                               features_comb=comb, write=write)
+            X_train, X_test, y_train, y_test = train_test_split(X, Y, shuffle=False,
+                                                                    test_size=test_size)
+            if train:
+                self.start_time, predicted = gs.predict(X_train, X_test, y_train, name, comb)
+            else:
+                class_name = comb[0] + '_' + comb[1] + '_' + comb[2]
+                loaded_model = pickle.load(
+                    open('rpeakdetection/KNN/classifiers/' + name + '_' + class_name + '.pkl', 'rb'))
+                self.start_time = time.time()
+                predicted = loaded_model.predict(X_test)
+            test_index = len(X_train) * window_size
+            elapsed_time, peaks = self.get_peaks(predicted, window_size, record, test_index, min_dist)
+            recall, precision = rpd.evaluate(peaks, path, evaluation_window_size, False, test_index)
+            print(recall)
+            print(precision)
+            recalls.append(recall)
+            precisions.append(precision)
+            times.append(elapsed_time)
+        return precisions, recalls, times
+
+    def get_sample_peaks(self, predicted, record):
+        predicted = np.where(predicted == 1)
         prev = 0
-        peaks = list()
-        all_peaks = np.array(all_peaks).flatten().tolist()
-        for p in all_peaks:
-            if p - prev > min_dist:
-                peaks.append(p)
-                prev = p
+        signal = record[0]
+        predicted = np.array(predicted).flatten().tolist()
+        regions = list()
+        region = list()
+        for p in predicted:
+            if p - prev == 1:
+                region.append(p)
+            else:
+                if prev!= 0:
+                    regions.append(region)
+                region = list([p])
+            prev = p
+        average = sum(list(map(lambda x: len(x), regions )))/len(regions)
+        filtered_regions =  list(filter(lambda x : len(x) >= average, regions))
+        peaks = [np.argmax(np.abs(signal[region[0]:region[-1]])) + region[0] for region in filtered_regions]
         return peaks
 
     def get_peaks(self, predicted_regions, window_size, record, test_index, min_dist):
@@ -108,7 +118,40 @@ class KNN:
         elapsed_time = elapsed_time/len(signal)
         return elapsed_time, Y_predicted
 
+    def SSK(self, comb, names, write,train, evaluation_window_size):
+        precisions= list()
+        recalls = list()
+        times = list()
+        # training is performed on the whole signal 100
+        train_path = ("data/ecg/" + self.DB + "/100")
+        train_rpeak_locations = ut.remove_non_beat(train_path, rule_based=False)[0]
+        record, X_train, y_train = fe.extract_features(name='100', path=train_path, rpeak_locations=train_rpeak_locations,
+                                           features_comb=comb, write=write)
+        if train:
+            model = gs.SSK_train(X_train, y_train, comb)
+        else:
+            model =  pickle.load(open('rpeakdetection/KNN/classifiers/SSK_'+comb[1]+'_'+comb[2]+'.pkl', 'rb'))
+        # testing is performed on all the other signals
+        for name in names[1:]:
+            path = ("data/ecg/" + self.DB +'/'+ name)
+            rpeak_locations = ut.remove_non_beat(path, rule_based=False)[0]
+            record, X_test, Y_test = fe.extract_features(name=name, path=path, rpeak_locations=rpeak_locations,
+                                                 features_comb=comb, write=write)
+            start_time = time.time()
+            predicted = model.predict(X_test)
+            print(sum(predicted))
+            elapsed_time = time.time() - start_time
+            elapsed_time = elapsed_time / len(record[0])
+            peaks = self.get_sample_peaks(predicted, record)
+            recall, precision = rpd.evaluate(peaks, path, evaluation_window_size, False)
+            print(recall)
+            print(precision)
+            recalls.append(recall)
+            precisions.append(precision)
+            times.append(elapsed_time)
+        return precisions, recalls, times
+
 
 if __name__ == '__main__':
     knn = KNN()
-    knn.rpeak_detection(write=True, train=True)
+    knn.rpeak_detection(write=False, train=False)