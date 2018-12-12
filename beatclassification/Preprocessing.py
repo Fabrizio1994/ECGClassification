3324f5fb2d12e95f24c44e839150fa5a8a015979
alescire94 <alescire94@gmail.com>
2018-12-12 11:38:23 +0100

    new segmentation method for NN

--- beatclassification/Preprocessing.py
+++ beatclassification/Preprocessing.py
@@ -207,10 +207,11 @@ class Preprocessing():
             beats[index] = beat
         return beats
 
-    def segment(self, classes):
+    def segment(self, classes, names, aami=True, one_hot=True):
         X = list()
         Y = list()
-        for name in ['232']:
+        for name in names:
+            X_signal = list()
             if name != '114':
                 channels = [0]
             else:
@@ -218,22 +219,17 @@ class Preprocessing():
             record = wfdb.rdrecord(ecg_path + name, channels=channels)
             record = np.transpose(record.p_signal)
             record = record[0]
-            record = self.remove_baseline(record)
-            peaks, symbols = ut.remove_non_beat(ecg_path + name, rule_based=False, include_VF=True)
-            data = list(filter(lambda x: x[1] in classes, zip(peaks, symbols)))
-            peaks, symbols = zip(*data)
+            peaks, symbols = ut.remove_non_beat(ecg_path + name, rule_based=False)
+            labels = self.extract_labels(aami, classes, one_hot, symbols)
             for i, p in enumerate(peaks[1:-1], start=1):
-                left_end = peaks[i - 1] + 20
-                right_end = peaks[i + 1] - 20
+                left_end = peaks[i ]-300
+                right_end = peaks[i]+300
                 beat = record[left_end:right_end]
-                label = symbols[i]
-                index = classes.index(label)
-                X.append(beat)
-                Y.append(index)
-        print(len(X))
-        print(len(Y))
-        print(set(Y))
-        return X, Y
+                label = np.argmax(labels[i])
+                X_signal.append(beat)
+                Y.append(label)
+            X.extend(X_signal)
+        return np.array(X), np.array(Y)
 
 
     def standardize(self, X_train, X_val, X_test):
@@ -303,4 +299,11 @@ class Preprocessing():
 
 
 
-
+if __name__ == '__main__':
+    pre = Preprocessing()
+    X, Y = (pre.segment(['N', 'S', 'V', 'F'], names = list(
+            {'100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '111', '112', '113', '114', '115',
+             '116', '117', '118', '119', '121', '122', '123', '124', '200', '201', '202', '203', '205', '207',
+             '208',
+             '209', '210', '212', '213', '214', '215', '217', '219', '220', '221', '222', '223', '228', '230', '231',
+             '232', '233', '234'} - {'102', '104', '107', '217', '207'})))
